57599d0c82a725bb449b1c2e1227f22aa249470e
Michael J <30031129@cc.peralta.edu>
2024-09-15 15:13:00 -0700

    changed cout and cin a little

---
+++ Questions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string street;
+    string city, state, zipcode;
+    cout << "what is your street name?" << endl;
+    getline(cin, street);
+    cout << "what is your city, state, and zipcode?" << endl;
+    cin >> city >> state >> zipcode;
+
+
+    return 0;
+}
